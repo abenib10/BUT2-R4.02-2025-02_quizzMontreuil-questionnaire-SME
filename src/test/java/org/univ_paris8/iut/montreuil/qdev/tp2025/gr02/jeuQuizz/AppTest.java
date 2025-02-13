1c2fdb647b2b360bf4279af5ae9638ffcff1f34d
abenib10 <benibrahimanass10@gmail.com>
2025-02-13 14:05:58 +0100

    commit initial

---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr02/jeuQuizz/AppTest.java
@@ -0,0 +1,20 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr02.jeuQuizz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
